cfe52dc84dbede76d013e6ebb0783ddbd1abe479
majidbhatti6312-oss <majidbhatti6312@gmail.com>
2026-04-08 23:39:34 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.baryaaaa.app',
+  appName: 'د بریا راز',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
