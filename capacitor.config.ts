099571da34427a4eddd078262b65fd1f876785cc
majidbhatti6312-oss <majidbhatti6312@gmail.com>
2026-04-09 02:57:46 -0400

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.amanmedia.umrah',
-  appName: 'د عمرې لارښود',
+  appId: 'com.obaidapp.iea.barya',
+  appName: 'د اسلامي امارت د بريا راز',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
